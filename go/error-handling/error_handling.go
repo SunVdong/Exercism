280a5f02b7035bbb61c797df2f4692e55211d2ea
sunweidong <sunvdong@qq.com>
2022-09-21 18:18:58 +0800

    新增错误处理

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,5 +1,37 @@
 package erratum
 
-func Use(opener ResourceOpener, input string) error {
-	panic("Please implement the Use function")
+import (
+	"errors"
+)
+
+func Use(opener ResourceOpener, input string) (a error) {
+	var res Resource
+	var err error
+
+	res, err = opener()
+	for err != nil && errors.As(err, &TransientError{}) {
+		res, err = opener()
+	}
+	if err != nil {
+		return err
+	}
+	defer res.Close()
+
+	defer func() {
+		if e := recover(); e != nil {
+			if er, ok := e.(*FrobError); ok {
+				res.Defrob(er.defrobTag)
+				a = er
+				return
+			} else {
+				a = errors.New("aaa")
+				return
+			}
+		} else {
+			return
+		}
+	}()
+
+	res.Frob(input)
+	return
 }
