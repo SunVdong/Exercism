13e64c1735def1f80da11713e3f76ab3d430a811
sunweidong <sunvdong@qq.com>
2022-09-22 15:59:42 +0800

    完成了

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -4,34 +4,40 @@ import (
 	"errors"
 )
 
-func Use(opener ResourceOpener, input string) (a error) {
-	var res Resource
-	var err error
-
-	res, err = opener()
+func Use(opener ResourceOpener, input string) (res_err error) {
+	res, err := opener()
 	for err != nil && errors.As(err, &TransientError{}) {
 		res, err = opener()
 	}
 	if err != nil {
-		return err
+		res_err = err
+		return
 	}
-	defer res.Close()
 
 	defer func() {
-		if e := recover(); e != nil {
-			if er, ok := e.(*FrobError); ok {
-				res.Defrob(er.defrobTag)
-				a = er
-				return
-			} else {
-				a = errors.New("aaa")
-				return
+		if r := recover(); r != nil {
+			switch e := r.(type) {
+			case FrobError:
+				res.Defrob(e.defrobTag)
+				res_err = e.inner
+			default:
+				res_err = r.(error)
 			}
-		} else {
-			return
 		}
+
+		res.Close()
+
+		// msg := recover()
+		// er, ok := msg.(FrobError)
+		// if ok {
+		// 	res.Defrob(er.defrobTag)
+		// 	res_err = er
+		// } else {
+		// 	res_err, _ = msg.(error)
+		// }
+		// res.Close()
 	}()
 
 	res.Frob(input)
-	return
+	return res_err
 }
