16a6c59ceb03db8d55a383ab932115f66306b202
sunweidong <sunvdong@qq.com>
2022-10-08 20:47:09 +0800

    修改了第n个质数的算法

--- go/nth-prime/nth_prime.go
+++ go/nth-prime/nth_prime.go
@@ -2,28 +2,29 @@ package prime
 
 import "errors"
 
-// Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
-func Nth(n int) (int, error) {
-	if n <= 0 {
-		return 0, errors.New("nth must be greater than zero")
-	}
-
-	count := 0
-	for i := 2; ; i++ {
-		if isPrime(i) {
-			count++
-		}
-		if count == n {
-			return i, nil
-		}
-	}
-}
+var primes = []int{2}
 
 func isPrime(n int) bool {
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
+	for _, p := range primes {
+		if n%p == 0 {
 			return false
 		}
 	}
 	return true
+}
+
+func Nth(n int) (int, error) {
+	if n < 1 {
+		return -1, errors.New("invalid input")
+	}
+	if n == 1 {
+		return 2, nil
+	}
+
+	for i := 3; len(primes) < n; i++ {
+		if isPrime(i) {
+			primes = append(primes, i)
+		}
+	}
+	return primes[len(primes)-1], nil
 }
