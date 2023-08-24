3428f428d993b0db399a8ff7626033cd3d1ec1c6
sunweidong <sunvdong@qq.com>
2023-08-24 10:43:09 +0800

    没看懂，抄了代码

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,27 +1,32 @@
-use rayon::prelude::*;
 use std::collections::HashMap;
-
+use std::thread;
+fn count_chars(text: &[&str]) -> HashMap<char, usize> {
+    let mut map = HashMap::new();
+    text.iter()
+        .flat_map(|&line| line.chars())
+        .filter(|ch| ch.is_alphabetic())
+        .filter_map(|ch| ch.to_lowercase().next())
+        .for_each(|ch| *map.entry(ch).or_default() += 1);
+    map
+}
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    (0..worker_count)
-        .into_par_iter()
-        .map(|i| {
-            let mut tallies = HashMap::new();
-            input
-                .join("")
-                .chars()
-                .skip(i)
-                .step_by(worker_count)
-                .filter(|c| c.is_alphabetic())
-                .flat_map(|c| c.to_lowercase())
-                .for_each(|c| {
-                    *tallies.entry(c).or_insert(0) += 1;
-                });
-            tallies
-        })
-        .reduce(HashMap::new, |mut result, m| {
-            m.iter().for_each(|(&k, &v)| {
-                *result.entry(k).or_insert(0) += v;
-            });
-            result
-        })
+    match input.len() {
+        0 => HashMap::new(),
+        n if n < 500 => count_chars(input),
+        l => {
+            thread::scope(|s| {
+                let mut map = HashMap::new();
+                let mut threads = Vec::with_capacity(worker_count);
+                for chunk in input.chunks(l / worker_count + 1) {
+                    threads.push(s.spawn(|| count_chars(chunk)));
+                }
+                for thread in threads {
+                    thread.join().unwrap().iter().for_each(|(&ch, &count)| {
+                        map.entry(ch).and_modify(|c| *c += count).or_insert(count);
+                    });
+                }
+                map
+            })
+        }
+    }
 }
