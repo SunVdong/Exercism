1f6b9622451bd9d2a4fb8503c6b9cefc9deba10a
vdong <sunvdong@qq.com>
2024-07-19 03:25:17 +0800

    正则解法

--- go/wordy/wordy.go
+++ go/wordy/wordy.go
@@ -1,61 +1,37 @@
 package wordy
 
 import (
+	"regexp"
 	"strconv"
-	"strings"
 )
 
-func Answer(question string) (int, bool) {
-	// replacer := strings.NewReplacer(
-	// 	"What is ", "",
-	// 	" by", "",
-	// 	"?", "",
-	// )
-	// question = replacer.Replace(question)
-
-	question = strings.TrimPrefix(question, "What is ")
-	question = strings.TrimSuffix(question, "?")
-	question = strings.ReplaceAll(question, " by", "")
-
-	var sum int
-	var opt string
-
-	has_left := false
-	for _, word := range strings.Fields(question) {
-		if n, err := strconv.Atoi(word); err == nil {
-			if !has_left {
-				sum = n
-				has_left = true
-				continue
-			}
-			if has_left {
-				switch opt {
-				case "plus":
-					sum += n
-				case "minus":
-					sum -= n
-				case "divided":
-					sum /= n
-				case "multiplied":
-					sum *= n
-				default:
-					return 0, false
-				}
-				opt = ""
-			}
-		} else {
-			if !has_left {
-				return 0, false
-			}
-			if opt != "" {
-				return 0, false
-			}
-			opt = word
-		}
+func Answer(s string) (int, bool) {
+	if match, _ := regexp.MatchString(`What is -?\d+(?: (?:plus|minus|divided by|multiplied by) -?\d+)*\?`, s); !match {
+		return 0, false
 	}
 
-	if opt != "" {
+	re1 := regexp.MustCompile(`(plus|minus|divided|multiplied)`)
+	operators := re1.FindAllString(s, -1)
+
+	re2 := regexp.MustCompile(`-?\d+`)
+	numbers := re2.FindAllString(s, -1)
+
+	if len(operators) != len(numbers)-1 {
 		return 0, false
+	}
+	sum, _ := strconv.Atoi(numbers[0])
+	for i, o := range operators {
+		n, _ := strconv.Atoi(numbers[i+1])
+		switch o {
+		case "plus":
+			sum += n
+		case "minus":
+			sum -= n
+		case "divided":
+			sum /= n
+		case "multiplied":
+			sum *= n
+		}
 	}
 
 	return sum, true
