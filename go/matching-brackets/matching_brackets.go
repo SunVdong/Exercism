d73e0024127d341297d4bcb76cdc727ad8f56cc8
孙维栋 <sunweidong@tesoon.com>
2025-07-24 18:08:28 +0800

    增加了托号匹配

---
+++ go/matching-brackets/matching_brackets.go
@@ -0,0 +1,29 @@
+package brackets
+
+func Bracket(input string) bool {
+	stack := []rune{}
+
+	for _, char := range input {
+		switch char {
+		case '(', '[', '{':
+			stack = append(stack, char)
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
